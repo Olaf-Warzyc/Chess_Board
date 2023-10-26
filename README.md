1945ff9e2e33097cf68b9692967f7bb2b73314d0
Replit user <>
2023-10-26 14:56:11 +0000

    add config files and libaries

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ this is work for the project this will include a virtual chess board with all th
 the pictures of pieces are from https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent it is free media, accessed on 26/10/2023 15:08
 
 other integrations like communications with camera and the Arduino will be done on a later date 
+
+[![Run on Repl.it](https://replit.com/badge/github/Olaf-Warzyc/Chess_Board)](https://replit.com/new/github/Olaf-Warzyc/Chess_Board)
--- main.py
+++ main.py
@@ -7,52 +7,49 @@ from pygame.locals import QUIT
 global screen, width, height
 global listx
 global listy
-listx = [] *8
-listy = [] *8
-pygame.init 
+listx = [] * 8
+listy = [] * 8
+pygame.init
 width = 480
-height =480
+height = 480
 clock = pygame.time.Clock()
-background_colour = (255,255,255) #set the backround colour 
-screen = pygame.display.set_mode((width,height)) # sets the 
+background_colour = (255, 255, 255)  #set the backround colour
+screen = pygame.display.set_mode((width, height))  # sets the
 pygame.display.set_caption('Chess')
-screen.fill(background_colour) #fills the backround as white 
-
+screen.fill(background_colour)  #fills the backround as white
 
 
 def drawGrid():
   i = 0
-  green = (0,128,0)
-  white = (255,255,255)
-  width_a= int(width/8)
-  height_a=int (height/8) 
-  for x in range(0, width , width_a):
-      for y in range(0, height, height_a ):
-          rect = pygame.Rect(x, y, width/8, height/8)
-          if x not in listx :
-            listx.append(x)
-          if y not in listy:
-            listy.append(y)
-          if (i%2 == 0):
-            pygame.draw.rect(screen, white, rect,0 )
-            i+=1
-          else :
-            pygame.draw.rect(screen, green, rect,0)
-            i+=1
-      i-=1
+  green = (0, 128, 0)
+  white = (255, 255, 255)
+  width_a = int(width / 8)
+  height_a = int(height / 8)
+  for x in range(0, width, width_a):
+    for y in range(0, height, height_a):
+      rect = pygame.Rect(x, y, width / 8, height / 8)
+      if x not in listx:
+        listx.append(x)
+      if y not in listy:
+        listy.append(y)
+      if (i % 2 == 0):
+        pygame.draw.rect(screen, white, rect, 0)
+        i += 1
+      else:
+        pygame.draw.rect(screen, green, rect, 0)
+        i += 1
+    i -= 1
 
-  return (listx,listy)
+  return (listx, listy)
+
+
 drawGrid()
-print (listy,listx)
-Gamepieces(screen,listx,listy)
-
-
+print(listy, listx)
+Gamepieces(screen, listx, listy)
 
 while True:
-    for event in pygame.event.get():
-       if event.type == QUIT:
-           pygame.quit()
-           sys.exit()
-    pygame.display.update()
-
-
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  pygame.display.update()
